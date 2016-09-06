33af58924f2d3ef55477061fce8d4c948871d688
Mariusz Roliński <mariusz@mondoreale.pl>
2016-09-06 15:38:45 +0200

    Add multiple scenarios; update docker image versions

--- test/test/test.js
+++ test/test/test.js
@@ -1,14 +1,27 @@
 var babl = require('./babl');
 var expect = require('chai').expect;
+var fs = require('fs');
+var readFile = require('bluebird').promisify(fs.readFile, fs);
 
 describe('Module', function() {
-  it('succeeds', function(done) {
-    babl('larskluge/string-upcase', { stdin: 'lorem ipsum' })
-      .then(function(result) {
-        var text = new Buffer(result.result.Stdout, 'base64').toString();
-        expect(text).to.equal('LOREM IPSUM');
-        done();
-      })
-      .catch(done);
+  ['string_upcase', 'string_upcase_10m'].forEach(function(filename) {
+    it('Successfully processes ' + filename, function(done) {
+      readFile('/data/expected/in/' + filename)
+        .then(function(input) {
+          return babl('larskluge/string-upcase', { stdin: input })
+            .then(function(result) {
+              if (result.error) {
+                throw new Error(result.error);
+              } else {
+                var buf = Buffer.from(result.result.Stdout, 'base64');
+                return readFile('/data/expected/out/' + filename)
+                  .then(function(output) {
+                    expect(Buffer.compare(buf, output)).to.equal(0);
+                    done();
+                  });
+              }
+            });
+        }).catch(done);
+    });
   });
-})
+});
